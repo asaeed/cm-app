a2c8beb832733c1e707a410bb7765af6564f07bf
Ahmad Saeed <asaeed@gmail.com>
2019-10-02 15:20:18 -0400

    move button color to .env

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 9999
 
 app.get('/', (req, res) => {
-  console.log('serving cm-app')
+  console.log('serving cm-app with', process.env.BUTTON_COLOR)
   res.send(`
-    <input type="button" value="Big Button" style="margin: auto; position: absolute; outline: none; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: cornflowerblue;">
+    <input type="button" value="Big Button" style="margin: auto; position: absolute; outline: none; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: ${process.env.BUTTON_COLOR};">
   `)
 })
 
