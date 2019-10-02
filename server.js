82892a45b4b77e4836427b0b44fa0d7ba4e432c8
Ahmad Saeed <asaeed@gmail.com>
2019-10-02 13:22:00 -0400

    add healthcheck

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => res.send(`
   <input type="button" value="Big Button" style="margin: auto; position: absolute; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: cornflowerblue;">
 `))
 
+app.get('/healthcheck', (req, res) => {
+  return res.status(200).send('OK')
+})
+
 app.listen(port, () => console.log(`cm-app listening on port ${port}`))
