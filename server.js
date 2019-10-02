da36bee62462d4db1c23162741078d05d69ba1eb
Ahmad Saeed <asaeed@gmail.com>
2019-10-02 14:12:07 -0400

    some logs

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 9999
 
-app.get('/', (req, res) => res.send(`
-  <input type="button" value="Big Button" style="margin: auto; position: absolute; outline: none; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: cornflowerblue;">
-`))
+app.get('/', (req, res) => {
+  console.log('serving cm-app')
+  res.send(`
+    <input type="button" value="Big Button" style="margin: auto; position: absolute; outline: none; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: cornflowerblue;">
+  `)
+})
 
 app.get('/healthcheck', (req, res) => {
   return res.status(200).send('OK')
