13321c32030ad4d9ab1988d80618ae207c82bc52
Ahmad Saeed <asaeed@gmail.com>
2019-10-02 13:18:22 -0400

    first

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 9999
+
+app.get('/', (req, res) => res.send(`
+  <input type="button" value="Big Button" style="margin: auto; position: absolute; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: cornflowerblue;">
+`))
+
+app.listen(port, () => console.log(`cm-app listening on port ${port}`))
