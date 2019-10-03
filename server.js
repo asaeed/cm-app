dd72afed33cba220213d86eb51628bd58b1ca163
Ahmad Saeed <asaeed@gmail.com>
2019-10-03 14:27:47 -0400

    log with timestamp so it shows up in syslogsd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9999
 
 app.get('/', (req, res) => {
-  console.log('serving cm-app with', process.env.BUTTON_COLOR, Math.random())
+  console.log('serving cm-app with', process.env.BUTTON_COLOR, new Date())
   res.send(`
     <input type="button" value="Big Button" style="margin: auto; position: absolute; outline: none; width: 200px; height: 50px; top: 0; right: 0; bottom: 0; left: 0; border-radius: 10px; color: white; background-color: ${process.env.BUTTON_COLOR};">
   `)
